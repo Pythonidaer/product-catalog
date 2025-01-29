eeaa6c15b3cf9f537039f0d21c5061bc8dfc2968
Jonathan Hammond <codefolio.work@gmail.com>
2025-01-28 19:46:46 -0500

    updated package.json and index.js so fe and be ports differ

--- app/index.js
+++ app/index.js
@@ -17,5 +17,5 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
